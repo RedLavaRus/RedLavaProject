8d59e8c47ba50d1de969647464d67e345a840e0c
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2020-11-29 17:28:58 +0300

    fix reg

--- core.rl.ru/modules/auth/controller/reg.php
+++ core.rl.ru/modules/auth/controller/reg.php
@@ -40,7 +40,14 @@ class Reg
         if($url["post"]["pass1"] != $url["post"]["pass2"] ){
             \Core\User\CollectorError::$error_pass .= "Пароли не совпадают";
             return "error:Пароли не совпадают";}
+            var_dump($url);
+        if(filter_var($url["post"]["email"], FILTER_VALIDATE_EMAIL) != true)
+            {
+                \Core\User\CollectorError::$error_email .= "Неверный емаил";
+                return "error:Неверный емаил";
+            }
         
+
         return self::cash($url);
         //проверить заполнены ли поля
     }
