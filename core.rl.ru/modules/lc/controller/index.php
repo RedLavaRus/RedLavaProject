010463b92ecd497baa00ed169eb950b4441adac3
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2021-01-02 22:40:23 +0300

    add new structur

--- core.rl.ru/modules/lc/controller/index.php
+++ core.rl.ru/modules/lc/controller/index.php
@@ -1,1 +1,10 @@
 <?php
+namespace Modules\Lc\Controller;
+
+
+use Core\Orm\Orm as Orm;
+use Core\Orm\Create as Create;
+
+class Index
+{
+}
---
+++ core.rl.ru/modules/lc/controller/service.php
@@ -0,0 +1,25 @@
+<?php
+namespace Modules\Lc\Controller;
+
+
+use Core\Orm\Orm as Orm;
+use Core\Orm\Create as Create;
+
+class Service
+{
+    public function swonBlockChangeFIO(){
+
+    }
+    public function swonBlockChangePass(){
+
+    }
+    public function swonBlockCreateUser(){
+
+    }
+    public function swonBlockUserList(){
+
+    }
+    public function swonBlocHistoryContactList(){
+
+    }
+}
