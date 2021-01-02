db2afed10c82be534e255918c5bfc7c90a4ecab1
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2021-01-02 14:41:00 +0300

    small fix

--- core.rl.ru/core/permission/config/right.php
+++ core.rl.ru/core/permission/config/right.php
@@ -11,12 +11,17 @@ class Right
 
        return  in_array($right,$res);
     }
+
     public static  function displayRight(){
         if(isset($_SESSION["id"]) and $_SESSION["id"] >= 1){
 
         }else{
             \Core\Errors\E403::show();
         }
+    }
+
+    public static function trueOrDie($right){
+        if(!self::accessRights($right)) die();
     }
 
 
