39a319efc7d2a4c77fb1216c14748dc98ae347a6
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2021-02-01 23:33:42 +0300

    t

--- core.rl.ru/template/defaults/crmphone/thinks.php
+++ core.rl.ru/template/defaults/crmphone/thinks.php
@@ -7,8 +7,24 @@
 
 <div >
 
-<table>
-<tr><td>#БД</td><td>ФИО</td><td>ДАТА НАЧАЛА</td><td>ПОКАЗАТЬ КЛИЕНТА</td><td>ЗАКРЫТЬ КЛИЕНТА</td></tr>
+<table class="tr_acm">
+<tr ><td class="head_acm">#Номер</td><td class="head_acm">ФИО</td><td class="head_acm">Дата последнего<br>звонка</td><td class="head_acm">Показать<br>клиента</td><td class="head_acm">Удалить<br>клиента</td><td class="head_acm">Статус</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_red">Просрочен</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue2"><img src="/template/defaults/img/crmphone/eye.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_green">Сейчас</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_yellow">Рано</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_red">Просрочен</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue2"><img src="/template/defaults/img/crmphone/eye.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_green">Сейчас</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_yellow">Рано</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_red">Просрочен</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue2"><img src="/template/defaults/img/crmphone/eye.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_green">Сейчас</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_yellow">Рано</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_red">Просрочен</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue2"><img src="/template/defaults/img/crmphone/eye.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_green">Сейчас</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_yellow">Рано</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_red">Просрочен</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue2"><img src="/template/defaults/img/crmphone/eye.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_green">Сейчас</td></tr>
+<tr class="tr_acm"><td>001</td><td>Иванов Иван Иванович</td><td>10.01.2021</td><td class="blue1"><img src="/template/defaults/img/crmphone/blink.png"></td><td class="close"><img src="/template/defaults/img/crmphone/cancel.png"></td><td class="status_yellow">Рано</td></tr>
+
 
 </table>
 
